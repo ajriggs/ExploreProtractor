087f545d738bf30352c3dba0b55b7a2ef5fb1b60
ajriggs <riggs.aaron@gmail.com>
2017-08-17 14:46:09 -0700

    Delete conf.js

--- conf.js
+++
@@ -1,9 +0,0 @@
-//config.js
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js'],
-  capabilities: {
-    browserName: 'chrome'
-  }
-};
